1114475b84e954be78556ef4ee4527ecb96f1345
Jose-Ulisses <joseulissesbuenoo@gmail.com>
2026-07-01 16:13:59 -0300

    fix: update mvnw and mvnw.cmd

--- mvnw.cmd
+++ mvnw.cmd
@@ -1,5 +1,12 @@
 @echo off
-setlocal
-set DIRNAME=%~dp0
-java -jar "%DIRNAME%\.mvn\wrapper\maven-wrapper.jar" %*
+setlocal enabledelayedexpansion
+set "DIRNAME=%~dp0"
+rem ensure trailing backslash
+if not "%DIRNAME:~-1%"=="\" set "DIRNAME=%DIRNAME%\"
+set "WRAPPER_JAR=%DIRNAME%\.mvn\wrapper\maven-wrapper.jar"
+if not exist "%WRAPPER_JAR%" (
+	echo Downloading maven-wrapper.jar...
+	powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://repo1.maven.org/maven2/io/takari/maven-wrapper/0.5.6/maven-wrapper-0.5.6.jar','%WRAPPER_JAR%')"
+)
+java -jar "%WRAPPER_JAR%" %*
 endlocal
